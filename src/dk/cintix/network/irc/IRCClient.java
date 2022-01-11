c357e0947741ee7eeec676778aa4f27dec7f49d4
Michael Martinsen <migo@tv2.dk>
2022-01-11 21:37:28 +0100

    dot dot

--- src/dk/cintix/network/irc/IRCClient.java
+++ src/dk/cintix/network/irc/IRCClient.java
@@ -1,9 +1,18 @@
 package dk.cintix.network.irc;
 
+import dk.cintix.network.irc.handler.IRCStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Michael Martinsen
  */
 public class IRCClient {
+    private final List<IRCStream> handlers = new ArrayList<>();
+    
+    public void addHandler(IRCStream stream){
+        handlers.add(stream);
+    }
     
 }
